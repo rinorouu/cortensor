57342df14ed1d5b3ebab54ba75e8dfb76194b518
rinorouu <rinorouu@gmail.com>
2025-02-28 02:12:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
-set -e  # Hentikan script jika ada error
+cat << "EOF"
+  ___                       ___           ___     
+     /  /\        ___          /__/\         /  /\    
+    /  /::\      /  /\         \  \:\       /  /::\   
+   /  /:/\:\    /  /:/          \  \:\     /  /:/\:\  
+  /  /:/~/:/   /__/::\      _____\__\:\   /  /:/  \:\ 
+ /__/:/ /:/___ \__\/\:\__  /__/::::::::\ /__/:/ \__\:\
+ \  \:\/:::::/    \  \:\/\ \  \:\~~\~~\/ \  \:\ /  /:/
+  \  \::/~~~~      \__\::/  \  \:\  ~~~   \  \:\  /:/ 
+   \  \:\          /__/:/    \  \:\        \  \:\/:/  
+    \  \:\         \__\/      \  \:\        \  \::/   
+     \__\/                     \__\/         \__\/    
 
-echo "Cloning Cortensor installer..."
+EOF
+set -e  # Exit immediately if any command fails
+
+# Install git if not exists
+if ! command -v git &> /dev/null; then
+    apt-get update && apt-get install -y git
+fi
+
+# Clone repository
+echo "Cloning repository..."
 git clone https://github.com/cortensor/installer
-
-# Pastikan folder 'installer' sudah ada sebelum lanjut
-while [ ! -d "installer" ]; do
-    echo "Waiting for installer folder..."
-    sleep 2
-done
-
 cd installer
 
-echo "Installing dependencies..."
-chmod +x install-docker-ubuntu.sh install-ipfs-linux.sh install-linux.sh
+# Run installation scripts
+echo "Installing Docker..."
 ./install-docker-ubuntu.sh
+
+echo "Installing IPFS..."
 ./install-ipfs-linux.sh
+
+echo "Installing Cortensor..."
 ./install-linux.sh
+
 cd
 
-echo "Setting up deploy user directory..."
+# Copy files and set permissions
+echo "Setting up deploy user..."
 cp -Rf ./installer /home/deploy/installer
 chown -R deploy:deploy /home/deploy/installer
 
-echo "Switching to deploy user..."
-sudo -u deploy bash <<EOF
-cd ~/
+# Verify installations as deploy user
+echo "Verifying installations..."
+sudo -u deploy sh -c 'cd ~ && ls -al /usr/local/bin/cortensord'
+sudo -u deploy sh -c 'cd ~ && ls -al $HOME/.cortensor/bin/cortensord'
+sudo -u deploy sh -c 'cd ~ && ls -al /etc/systemd/system/cortensor.service'
+sudo -u deploy sh -c 'cd ~ && ls -al $HOME/.cortensor/bin/start-cortensor.sh'
+sudo -u deploy sh -c 'cd ~ && ls -al $HOME/.cortensor/bin/stop-cortensor.sh'
 
-echo "Checking installation files..."
-ls -al /usr/local/bin/cortensord
-ls -al \$HOME/.cortensor/bin/cortensord
-ls -al /etc/systemd/system/cortensor.service
-ls -al \$HOME/.cortensor/bin/start-cortensor.sh
-ls -al \$HOME/.cortensor/bin/stop-cortensor.sh
+# Check Docker and IPFS versions
+echo "Docker Version: $(docker --version)"
+echo "IPFS Version: $(ipfs --version)"
 
-echo "Checking Docker and IPFS versions..."
-docker version
-ipfs version
+# Generate key as deploy user
+echo "Generating keys..."
+sudo -u deploy /usr/local/bin/cortensord /home/deploy/.cortensor/.env tool gen_key
 
-echo "Generating key for Cortensor..."
-/usr/local/bin/cortensord \$HOME/.cortensor/.env tool gen_key
-EOF
-
-echo "Installation completed!"
+echo "Installation completed successfully!"
